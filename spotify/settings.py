382619b5ae7f4a7c0de357aa89112407d6651379
johnayinde <lolaayinde@gmail.com>
2024-07-08 15:20:51 +0100

    allowed host

--- spotify/settings.py
+++ spotify/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.railway.com','127.0.0.1']
+ALLOWED_HOSTS = '*'
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 # Application definition
