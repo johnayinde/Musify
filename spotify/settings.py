f576ae5b77fc8b4bfa9578c810f7bb19dddf0a38
johnayinde <lolaayinde@gmail.com>
2024-07-08 15:23:43 +0100

    ALLOWED_HOSTS

--- spotify/settings.py
+++ spotify/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = ['.render.com','127.0.0.1', '*']
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 # Application definition
